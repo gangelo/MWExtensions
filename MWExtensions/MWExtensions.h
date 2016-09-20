14c26ee05fc4ab91965e64bccd581fa9cede8f91
gangelo <web.gma@gmail.com>
2016-09-20 09:54:58 -0400

    Initial commit

---
+++ MWExtensions/MWExtensions.h
@@ -0,0 +1,19 @@
+//
+//  MWExtensions.h
+//  MWExtensions
+//
+//  Created by Gene M. Angelo  Jr. on 9/20/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWExtensions.
+FOUNDATION_EXPORT double MWExtensionsVersionNumber;
+
+//! Project version string for MWExtensions.
+FOUNDATION_EXPORT const unsigned char MWExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWExtensions/PublicHeader.h>
+
+
---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+import MWExtensions
+
+var myString:String? = "\t\n"
+
+myString.isNilOrEmpty()
+myString.trim()
