72e5e911dc3e46800e1b9ed21b10b85ef79288bc
Gene M. Angelo, Jr <web.gma@gmail.com>
2016-11-17 12:41:32 -0500

    Create String+trim.swift

---
+++ MWExtensions/String+trim.swift
@@ -0,0 +1,14 @@
+//
+//  String+trim.swift
+//
+//  Created by Gene M. Angelo  Jr. on 9/13/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+import Foundation
+
+extension String {
+    
+    func trim() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+}
